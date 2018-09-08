cadd2aad16209c78139a112091fc4ece0774574a
NH1922 <31303415+NH1922@users.noreply.github.com>
2018-09-08 13:13:22 +0530

    Added more layers

--- train.py
+++ train.py
@@ -16,20 +16,29 @@ classifier = Sequential()
 
 
 classifier.add(Convolution2D(filters=32,kernel_size=(3,3),
-                             input_shape=(64,64,3),
+                             input_shape=(128,128,3),
+                             activation ='relu'))
+
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+
+# second convolution layer
+classifier.add(Convolution2D(filters=32,kernel_size=(3,3),
+                             input_shape=(100,100,3),
                              activation ='relu'))
 
 classifier.add(MaxPooling2D(pool_size=(2,2)))
 
 classifier.add(Flatten())
 
-classifier.add(Dense(units = 32,activation='relu'))
+classifier.add(Dense(units = 128,activation='relu'))
 
-#number of neurons = 2 for output layer because current dataset has 2 guns
-classifier.add(Dense(units = 2,activation = 'softmax'))
+classifier.add(Dense(units = 128,activation='relu'))
+
+classifier.add(Dropout(0.2))
+
+classifier.add(Dense(units = 3,activation = 'softmax'))
+
 classifier.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
-
-
 
 train_datagen = ImageDataGenerator(rescale = 1./255,
                                    shear_range = 0.2,
@@ -40,21 +49,25 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory(r"E:\Guns\Training",
-                                                 target_size = (100,100),
-                                                 batch_size = 10,
+training_set = train_datagen.flow_from_directory(r"E:\datasets\CSGO\train_set",
+                                                 target_size = (128,128),
+                                                 batch_size = 15,
                                                  class_mode = 'categorical')
 
-test_set = test_datagen.flow_from_directory(r"E:\Guns\Test",
-                                            target_size = (100, 100),
-                                            batch_size = 10,
+test_set = test_datagen.flow_from_directory(r"E:\datasets\CSGO\test_set",
+                                            target_size = (128, 128),
+                                            batch_size = 15,
                                             class_mode = 'categorical')
 
 
+classifier.summary()
+
 classifier.fit_generator(training_set,
                          steps_per_epoch = 1000,
-                         epochs = 10,
+                         epochs = 15,
                          validation_data = test_set,
                          validation_steps = 1000,workers=3)
-                         
-classifier.save(r"GunDet")
+
+
+classifier.save(r"E:\datasets\models\GunDet_v4")
+
